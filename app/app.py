2f127936767cd12b107af194862ebc8943b71a3d
flashdev53 <adevaraj53@gmail.com>
2025-08-27 19:05:17 +0000

    Update App.py

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Auto-Cloud CI/CD!"
+    return "Hello from Auto-Cloud CI/CD Done!"
 
 @app.route("/health")
 def health():
